d00bcb7561dfb0ef2eaf3da6f3ecfb8ea5fbf7a4
PSHSRobotics <33577367+PSHSRobotics@users.noreply.github.com>
2018-03-08 14:39:31 -0600

    I think it works...

--- lineFollower.c
+++ lineFollower.c
@@ -49,42 +49,49 @@ task main()
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
 		*/
 		// White is around ~0 and dark is ~4000
-    // If one of the sensors see white:
-    if(SensorValue(lineFollowerRIGHT) > threshold || SensorValue(lineFollowerCENTER) > threshold || SensorValue(lineFollowerLEFT) > threshold)
+    if(SensorValue(lineFollowerRIGHT) > threshold && SensorValue(lineFollowerCENTER) > threshold && SensorValue(lineFollowerLEFT) > threshold)
     {
     	//Sees dark then move
+      //Move Forward
       motor[leftMotor]  = 50;
       motor[rightMotor] = 50;
     }
-    else if(SensorValue(lineFollowerLEFT) < threshold || SensorValue(lineFollowerRIGHT) > threshold || SensorValue(lineFollowerCENTER) > threshold)
+    else if(SensorValue(lineFollowerLEFT) < threshold && SensorValue(lineFollowerRIGHT) > threshold && SensorValue(lineFollowerCENTER) > threshold)
     {
-    	// if white is seen by left sesor then left will move normal speed and the left motor will move at a slightly higher speed:)
+    	// if white is seen by left sensor then left will move normal speed and the left motor will move at a slightly higher speed:)
+      //Move left slightly
     	motor[leftMotor] = 50;
     	motor[rightMotor] = 55;
     }
-    else if(SensorValue(lineFollowerLEFT) > threshold || SensorValue(lineFollowerRIGHT) < threshold || SensorValue(lineFollowerCENTER) < threshold))
+    else if(SensorValue(lineFollowerLEFT) > threshold && SensorValue(lineFollowerRIGHT) < threshold && SensorValue(lineFollowerCENTER) < threshold)
   	{
   		// If black is seen by left sensor and white is seen by the right sensor the move right motor move higher speed
+  	  // Move right
   		motor[leftMotor] = 60;
   		motor[rightMotor] = 50;
   	}
-    else if(SensorValue(lineFollowerLEFT) > threshold || SensorValue(lineFollowerRIGHT) < threshold || SensorValue(lineFollowerCENTER) < threshold))
+    else if(SensorValue(lineFollowerLEFT) < threshold && SensorValue(lineFollowerRIGHT) > threshold && SensorValue(lineFollowerCENTER) < threshold)
   	{
   		// If black is seen by left sensor and white is seen by the right sensor the move right motor move higher speed
-  		motor[leftMotor] = 60;
-  		motor[rightMotor] = 50;
-  	}
-  	else if(SensorValue(lineFollowerLEFT) < threshold || SensorValue(lineFollowerRIGHT) > threshold || SensorValue(lineFollowerCENTER) < threshold)
+  	  // Moves left drastically
   		motor[leftMotor] = 50;
-  		motor[rightMotor] = 55;
-    else
-    	// Sees white then stop:
+  		motor[rightMotor] = 60;
+  	}
+  	else if(SensorValue(lineFollowerLEFT) > threshold && SensorValue(lineFollowerRIGHT) < threshold && SensorValue(lineFollowerCENTER) < threshold)
     {
-      // go straight
+      //Move Right Slightly
+      motor[leftMotor] = 55;
+  		motor[rightMotor] = 50;
+    }
+    else
+    {
+      // Sees white then stop:
       motor[leftMotor]  = 0;
       motor[rightMotor] = 0;
     }
+    
   }
+  
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //Got EM COACH LLLLLOOOOOLLLLLLLL LMAO
